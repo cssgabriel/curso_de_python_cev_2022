c79946eaba91f54cc3efc9927509ec5043f0c7cc
Gabriel Carlos <105656968+cssgabriel@users.noreply.github.com>
2022-06-28 13:27:02 -0300

    Add files via upload

---
+++ exercícios/Mundo 1/ex019.py
@@ -0,0 +1,10 @@
+# Exercício 19: Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+lista = [n1, n2, n3, n4]
+print("O escolhido foi {}".format(random.choice(lista)))
+
