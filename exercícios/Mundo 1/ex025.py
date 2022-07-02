4e61249cc0174ce0aafa818acf1e05e1a7dd39b3
Gabriel Carlos <105656968+cssgabriel@users.noreply.github.com>
2022-07-02 15:22:41 -0300

    Add files via upload

---
+++ exercícios/Mundo 1/ex025.py
@@ -0,0 +1,4 @@
+# Exercício 25. Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nome = str(input("Digite seu nome: ")).strip()
+print("Seu nome tem Silva? {}".format("SILVA" in nome.upper()))
